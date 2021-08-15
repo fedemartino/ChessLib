98bb539a730470dea984da48d01ae011254a3671
Federico Martino <fedemartino@gmail.com>
2021-08-14 21:41:51 -0300

    clean library

---
+++ ChessLib/Listeners/ICellSelectListener.cs
@@ -0,0 +1,14 @@
+﻿
+using ChessLib.Models;
+
+namespace ChessLib.Listeners
+{
+    public interface ICellSelectListener
+    {
+        /// <summary>
+        /// Método que se llama cuando se desea invocar el listener
+        /// </summary>
+        /// <param name="selectedCell">Coordenada de la celda seleccionada</param>
+        void Listen(Coordenada selectedCell);
+    }
+}
